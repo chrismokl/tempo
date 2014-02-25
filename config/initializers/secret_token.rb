c4e7a7e1e5f8205679dd205d9c57c2eb0f820114
Chris Mo <cmo871@gmail.com>
2014-02-24 19:40:48 -0800

    da

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tempo::Application.config.secret_key_base = '722cec7624f6b184592a149f0c75993d1bee8b591e6fcd67b89a018dab719dab99d7d21f6df6b2eb1105b5ab5ef4a114e426d276fbc3be13849e235f528008bb'
